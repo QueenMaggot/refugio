3f6286cdb0c8c53d6271f2f0d7ca52c9b30cc7f9
QueenMaggot <alvarezh.tamara@gmail.com>
2025-11-30 17:28:05 -0300

    settings agregue staticroot staticfiles

--- src/proyecto_adopciones/settings.py
+++ src/proyecto_adopciones/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static',]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#BASE_DIR / 'staticfiles'
 
 import os #Para las imagenes
 
