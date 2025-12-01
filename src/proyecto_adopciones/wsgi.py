cd14e4bc50939fb98527ae13f6885a9ab24fabd3
QueenMaggot <alvarezh.tamara@gmail.com>
2025-12-01 09:08:37 -0300

    initialdatav88

--- src/proyecto_adopciones/wsgi.py
+++ src/proyecto_adopciones/wsgi.py
@@ -8,12 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_adopciones.settings')
-
-from proyecto_adopciones.initial_data import load_initial_data_if_needed
-load_initial_data_if_needed()
 
 application = get_wsgi_application()
