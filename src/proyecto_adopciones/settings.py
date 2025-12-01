a7efde34ba1b07f42ce17307c5db2a7ccf372a57
QueenMaggot <alvarezh.tamara@gmail.com>
2025-12-01 09:40:05 -0300

    quemadisimav2

--- src/proyecto_adopciones/settings.py
+++ src/proyecto_adopciones/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     #Aplicaciones propias
-    'proyecto_adopciones'
+    'proyecto_adopciones',
     'animales',
     'adoptantes',
     'refugios',
